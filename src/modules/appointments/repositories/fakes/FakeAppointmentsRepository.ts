ff52154004e813590802b0753444be144fbf6c4e
leoferraari <leonardo.5567@hotmail.com>
2023-02-07 21:33:46 -0300

    Teste Criação de Usuário

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,5 +1,6 @@
 
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
@@ -12,8 +13,8 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
 
     public async findByDate(date: Date): Promise<Appointment | undefined> {
 
-        const findAppointment = this.appointments.find(
-          appointment => appointment.date == date,
+        const findAppointment = this.appointments.find( appointment =>
+          isEqual(appointment.date, date),
         );
 
         return findAppointment;
