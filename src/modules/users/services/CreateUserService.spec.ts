809c49271c52b653c90174e1dc55a6bf284243c5
leoferraari <leonardo.5567@hotmail.com>
2023-02-07 22:02:07 -0300

    Teste Criação de Usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
+import CreateUserService from "./CreateUserService";
+
+//categoria
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+    );
+
+    const user = await createUser.execute({
+      name: 'Leonardo',
+      email: 'leonardo.ferrari@unidavi.edu.br',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create anew user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+    );
+
+    await createUser.execute({
+      name: 'Leonardo',
+      email: 'leonardo.ferrari@unidavi.edu.br',
+      password: '123456',
+    });
+
+    expect(createUser.execute({
+        name: 'Leonardo',
+        email: 'leonardo.ferrari@unidavi.edu.br',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
+
