b57e888e1583197bf00e6d26134a0cc1c14dbcd6
leoferraari <leonardo.5567@hotmail.com>
2023-02-25 11:31:14 -0300

    Testes reset de Senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -12,11 +12,19 @@ class FakeUsersRepository implements IUserTokensRepository {
 
     Object.assign(userToken, {
       id: uuidv4(),
-      token: uuidv4(),
+      token: uuidv4(),  
       user_id,
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token == token,
+    )
 
     return userToken;
   }
---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,51 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+import SendForgotPasswrodEmailService from './SendForgotPasswordEmailService';
+import ResetPasswordService from './ResetPasswordService';
+
+  let fakeUsersRepository: FakeUsersRepository;
+  let fakeUserTokensRepository: FakeUserTokensRepository;
+  let resetPassword: ResetPasswordService;
+
+describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    resetPassword = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokensRepository
+    );
+
+
+  });
+
+  it('should be able to reset the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'leonardo.ferrari@unidavi.edu.br',
+      password: '123456'
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    await resetPassword.execute({
+      password: '123123',
+      token,
+    });
+
+    const updateUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updateUser ?.password).toBe('123123');
+  });
+
+  //hash
+  //2h expiração
+
+
+});
+
+// RED - Primeiro faz o teste falhar
+// GREEN - Fazer o teste dar certo.
+// REFATORAR
