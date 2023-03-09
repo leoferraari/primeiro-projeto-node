350222e0566503361561437f6fa01344cd1f6966
leoferraari <leonardo.5567@hotmail.com>
2023-03-09 20:13:06 -0300

    Template Engine - Email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -33,6 +34,17 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
+    console.log('Teste forgotPasswordTemplate: ' + forgotPasswordTemplate);
+    console.log('Teste dirname: ' + __dirname);
+
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -40,10 +52,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de Senha',
       templateData: {
-        template: 'Ólá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         }
       }
 
