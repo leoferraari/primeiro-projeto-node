46caecdf90f9ce187d653c0ee2e2d675293c6c77
leoferraari <leonardo.5567@hotmail.com>
2023-03-14 18:10:03 -0300

    Testes e Service atualização de perfil

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,117 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfile: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfile = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+  });
+
+  it('should be able update the profile', async () => {
+
+    const user = await fakeUsersRepository.create({
+      name: 'Leonardo 1',
+      email: 'leonardo.ferrari1@unidavi.edu.br',
+      password: '123456',
+    });
+
+    const updateUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'Leonardo 2',
+      email: 'leonardo.ferrari2@unidavi.edu.br'
+    });
+
+    expect(updateUser.name).toBe('Leonardo 2');
+    expect(updateUser.email).toBe('leonardo.ferrari2@unidavi.edu.br');
+  });
+
+  it('should not be able to change to another user email', async () => {
+
+    await fakeUsersRepository.create({
+      name: 'Leonardo 1',
+      email: 'leonardo.ferrari1@unidavi.edu.br',
+      password: '123456',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Teste',
+      email: 'teste@example.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Leonardo 1',
+        email: 'leonardo.ferrari1@unidavi.edu.br',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Leonardo 1',
+      email: 'leonardo.ferrari1@unidavi.edu.br',
+      password: '123456',
+    });
+
+    const updateUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'Leonardo 2',
+      email: 'leonardo.ferrari2@unidavi.edu.br',
+      old_password: '123456',
+      password: '123123'
+    });
+
+    expect(updateUser.password).toBe('123123');
+  });
+
+  it('should not be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Leonardo 1',
+      email: 'leonardo.ferrari1@unidavi.edu.br',
+      password: '123456',
+    });
+
+    expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Leonardo 2',
+        email: 'leonardo.ferrari2@unidavi.edu.br',
+        password: '123123'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Leonardo 1',
+      email: 'leonardo.ferrari1@unidavi.edu.br',
+      password: '123456',
+    });
+
+    expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'Leonardo 2',
+        email: 'leonardo.ferrari2@unidavi.edu.br',
+        old_password: 'wrong-old-password',
+        password: '123123'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
+
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -4,17 +4,22 @@ import FakeStorageProvider from '@shared/container/providers/StorageProvider/fak
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
 
 describe('UpdateUserAvatar', () => {
-  it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
 
-    const updateUserAvatar = new UpdateUserAvatarService(
+    updateUserAvatar = new UpdateUserAvatarService(
       fakeUsersRepository,
       fakeStorageProvider
     );
+  });
 
+  it('should be able to create a new user', async () => {
     const user = await fakeUsersRepository.create({
       name: 'Leonardo',
       email: 'leonardo.ferrari@unidavi.edu.br',
@@ -30,14 +35,6 @@ describe('UpdateUserAvatar', () => {
   });
 
   it('should not be able to update avatar from non existing user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
-    const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository,
-      fakeStorageProvider
-    );
-
     await expect(
       updateUserAvatar.execute({
         user_id: 'non-existing-user',
@@ -47,16 +44,8 @@ describe('UpdateUserAvatar', () => {
 
 
   it('should delete old avatar when updating new one', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     //consegue observer se determinada função foi executada.
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile'); //espionar
-
-    const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository,
-      fakeStorageProvider
-    );
 
     const user = await fakeUsersRepository.create({
       name: 'Leonardo',
