6a981d6d862ca273f9b8740e57c12b65ac00536d
leoferraari <leonardo.5567@hotmail.com>
2023-04-03 22:02:11 -0300

    Utilizando Cache ao listar os Providers

--- src/modules/appointments/services/LIstProvidersService.ts
+++ src/modules/appointments/services/LIstProvidersService.ts
@@ -1,4 +1,5 @@
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -12,14 +13,25 @@ class ListProviderService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
-    return user;
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+
+    return users;
   }
 }
 
