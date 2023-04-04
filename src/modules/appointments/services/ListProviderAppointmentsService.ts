6a73d7945872fe63b84ad38945c0d5b4fa329642
leoferraari <leonardo.5567@hotmail.com>
2023-04-03 21:14:16 -0300

    CacheProvider - Redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,3 +1,4 @@
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import { injectable, inject } from 'tsyringe';
 // import { getDaysInMonth, getDate } from 'date-fns';
 
@@ -16,15 +17,24 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ provider_id, day, month, year }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+
+    console.log(cacheData);
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year,
     });
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
